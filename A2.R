7229c334df8334b84748788d9c6d6d42cf52da9e
Jonathan Mostovoy <jmostovoy@gmail.com>
2017-04-04 16:56:21 -0400

    setwd

--- A2.R
+++ A2.R
@@ -1,1 +1,21 @@
-#A2#
+#A2
+### Preamble #############################
+library(stats)
+library(tseries)
+library(forecast)
+library(xtable)
+library(fGarch)
+library(FinTS)
+library(ggplot2)
+setwd("~/Documents/Mathematical_Finance")
+
+### Question 1 #############################
+
+
+
+
+### Question 2 #############################
+
+bell <- read.csv("bell_stocks_prices.csv")
+dbell_Adj.Close<-diff(bell$Adj.Close)
+sd(dbell_Adj.Close)
